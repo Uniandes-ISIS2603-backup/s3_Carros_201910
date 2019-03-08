df821c3f07909744d3ba7f661a815edb075b2fbd
Daniel Lozano <de.lozanoe@uniandes.edu.co>
2019-03-08 17:19:55 -0500

    Resouse completos

--- s3_carros-api/src/main/java/co/edu/uniandes/csw/carros/resources/RegistroCompraResourse.java
+++ s3_carros-api/src/main/java/co/edu/uniandes/csw/carros/resources/RegistroCompraResourse.java
@@ -6,9 +6,15 @@
 package co.edu.uniandes.csw.carros.resources;
 
 import co.edu.uniandes.csw.carros.dtos.RegistroCompraDTO;
+import co.edu.uniandes.csw.carros.ejb.RegistroCompraLogic;
+import co.edu.uniandes.csw.carros.entities.RegistroCompraEntity;
+import co.edu.uniandes.csw.carros.exceptions.BusinessLogicException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -16,6 +22,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -29,29 +36,65 @@ import javax.ws.rs.core.MediaType;
 public class RegistroCompraResourse 
 {
     private static final Logger LOGGER = Logger.getLogger(RegistroCompraResourse.class.getName());
+    @Inject
+    private RegistroCompraLogic logica;
     
     @POST
-    public RegistroCompraDTO createdRegistroCompra(RegistroCompraDTO registroCompra)
+    public RegistroCompraDTO createdRegistroCompra(RegistroCompraDTO registroCompra) throws BusinessLogicException
     {
-        return registroCompra;
+        LOGGER.log(Level.INFO, "RegistroCompraResourse createRegistroCompra: input: {0}", registroCompra);
+        // Convierte el DTO (json) en un objeto Entity para ser manejado por la lógica.
+        RegistroCompraEntity entity = registroCompra.toEntity();
+        // Invoca la lógica para crear la editorial nueva
+        RegistroCompraEntity nuevoRegistroCompraEntity = logica.createRegistroCompra(entity);
+        // Como debe retornar un DTO (json) se invoca el constructor del DTO con argumento el entity nuevo
+        RegistroCompraDTO nuevoRegistroCompraDTO = new RegistroCompraDTO(nuevoRegistroCompraEntity);
+        LOGGER.log(Level.INFO, "RegistroCompraResourse createRegistroCompra: output: {0}", nuevoRegistroCompraDTO);
+        return nuevoRegistroCompraDTO;
     }
     
     @GET
-    public ArrayList<RegistroCompraDTO> getRegistrosCompra()
+    public List<RegistroCompraDTO> getRegistrosCompra()
     {
-        return new ArrayList<>() ;
+        LOGGER.info("RegistroCompraResourse getRegistroCompra: input: void");
+        List<RegistroCompraDTO> listaEditoriales = listEntity2DTO(logica.getRegistrosCompra());
+        LOGGER.log(Level.INFO, "RegistroCompraResourse getRegistroCompra: output: {0}", listaEditoriales);
+        return listaEditoriales;
+    }
+    
+     private List<RegistroCompraDTO> listEntity2DTO(List<RegistroCompraEntity> entityList) {
+        List<RegistroCompraDTO> list = new ArrayList<>();
+        for (RegistroCompraEntity entity : entityList) {
+            list.add(new RegistroCompraDTO(entity));
+        }
+        return list;
     }
     
     @GET
     @Path("{compraID: \\d+}")
     public RegistroCompraDTO getCliente(@PathParam("compraID") Long registroCompraID){
-        return null;
+          LOGGER.log(Level.INFO, "RegistroCompraResourse getRegistroCompra: input: {0}", registroCompraID);
+        RegistroCompraEntity editorialEntity = logica.getRegistroCompra(registroCompraID);
+        if (editorialEntity == null) {
+            throw new WebApplicationException("El recurso /registroCompra/" + registroCompraID + " no existe.", 404);
+        }
+        RegistroCompraDTO DTO = new RegistroCompraDTO(editorialEntity);
+        LOGGER.log(Level.INFO, "RegistroCompraResourse getRegistroCompra: output: {0}", DTO);
+        return DTO;
     }
    
     @PUT
     @Path("{compraID: \\d+}")
-    public RegistroCompraDTO updateEmpleado(@PathParam("compraID") Long empleadoID, RegistroCompraDTO registroCompra){
-        return registroCompra;
+    public RegistroCompraDTO updateRegistroCompra(@PathParam("compraID") Long registroCompraID, RegistroCompraDTO registroCompra){
+         LOGGER.log(Level.INFO, "RegistroCompraResourse updateRegistroCompra: input: id:{0} , editorial: {1}", new Object[]{registroCompraID, registroCompra});
+        registroCompra.setCompraId(registroCompraID);
+        if (logica.getRegistroCompra(registroCompraID) == null) {
+            throw new WebApplicationException("El recurso /registroCompra/" + registroCompraID + " no existe.", 404);
+        }
+        RegistroCompraDTO DTO = new RegistroCompraDTO(logica.updateRegistroCompra(registroCompraID, registroCompra.toEntity()));
+        LOGGER.log(Level.INFO, "EditorialResource updateEditorial: output: {0}", DTO);
+        return DTO;
+
     }
    
 }
