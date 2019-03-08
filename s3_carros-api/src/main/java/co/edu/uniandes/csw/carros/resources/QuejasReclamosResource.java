85f533de6a5a604df4cb94d59c5ccd3028966d2d
Julio <juceruto@outlook.com>
2019-03-08 15:00:33 -0500

    correcion path

--- s3_carros-api/src/main/java/co/edu/uniandes/csw/carros/resources/QuejasReclamosResource.java
+++ s3_carros-api/src/main/java/co/edu/uniandes/csw/carros/resources/QuejasReclamosResource.java
@@ -53,7 +53,6 @@ public class QuejasReclamosResource {
 }
     
     @GET
-    //@Path("{casoId: \\d+}")
     public List<QuejasReclamosDTO> getQuejasReclamos() {
         LOGGER.info("QuejasReclamosResource QuejasReclamosBooks: input: void");
         List<QuejasReclamosDTO> listaQuejasReclamos = listEntity2DetailDTO(quejasReclamosLogic.getQuejasReclamos());
