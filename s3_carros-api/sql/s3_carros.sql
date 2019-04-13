deb6eb9b5b02b1bf28a91453552803533a1c0444
Daniel Lozano <de.lozanoe@uniandes.edu.co>
2019-04-12 19:35:11 -0500

    ..

--- s3_carros-api/sql/s3_carros.sql
+++ s3_carros-api/sql/s3_carros.sql
@@ -14,6 +14,23 @@ delete from PuntoVentaEntity;
 insert into PUNTOVENTAENTITY (id, direccion, telefono) values (1, 'Calle 10 #26-18', '3508484');
 insert into PUNTOVENTAENTITY (id, direccion, telefono) values (2, 'Calle 15 #30-18', '3508686');
 
+insert into MARCAENTITY (ID, DESCRIPCION, IMAGEN, NOMBREMARCA)
+values (1, 'Nissan Motor Company, 
+Limited es un fabricante japonés de automóviles, con base en Yokohama. Su nombre común, Nissan,
+ es un acrónimo de "Nippon Sangyo". Está entre las principales compañías automotrices en términos
+  de producción anual de vehículos',
+ 'https://banner2.kisspng.com/20180802/ivx/kisspng-logo-nissan-car-brand-emblem-nissan-maxima-workshop-service-amp-repair-manual-5b63b1ee7124e1.4901775015332602704635.jpg', 'Nissan');
+insert into MARCAENTITY (ID, DESCRIPCION, IMAGEN, NOMBREMARCA) values (2, null, null, 'GMC');
+insert into MARCAENTITY (ID, DESCRIPCION, IMAGEN, NOMBREMARCA) values (3, null, null, 'Ford');
+insert into MARCAENTITY (ID, DESCRIPCION, IMAGEN, NOMBREMARCA) values (4, null, null, 'Porsche');
+insert into MARCAENTITY (ID, DESCRIPCION, IMAGEN, NOMBREMARCA) values (5, null, null, 'Toyota');
+insert into MARCAENTITY (ID, DESCRIPCION, IMAGEN, NOMBREMARCA) values (6, null, null, 'Merdeces');
+insert into MARCAENTITY (ID, DESCRIPCION, IMAGEN, NOMBREMARCA) values (7, null, null, 'Maserati');
+insert into MARCAENTITY (ID, DESCRIPCION, IMAGEN, NOMBREMARCA) values (8, null, null, 'Audi');
+insert into MARCAENTITY (ID, DESCRIPCION, IMAGEN, NOMBREMARCA) values (9, null, null, 'BMW');
+insert into MARCAENTITY (ID, DESCRIPCION, IMAGEN, NOMBREMARCA) values (10, null, null, 'Mazda');
+
+
 insert into ClienteEntity (id, nombre, correo, telefono) values (1, 'Camilo Perez', 'cp@mail.com', '3124548978');
 insert into ClienteEntity (id, nombre, correo, telefono) values (2, 'Carla Espitia', 'ce@mail.com', '3124568168');
 
